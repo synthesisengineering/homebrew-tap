92de05e9a3069e917d36e6d3e5bee90be1e6a25e
Rajiv Pant <rajiv@rajiv.com>
2026-09-18 11:16:52 -0400

    synthesis 4.102.1

--- Formula/synthesis.rb
+++ Formula/synthesis.rb
@@ -1,8 +1,8 @@
 class Synthesis < Formula
   desc "Verified installation and lifecycle for the Synthesis ecosystem"
   homepage "https://synthesiswork.org/download/"
-  url "https://github.com/synthesisengineering/synthesis-skills/releases/download/v4.101.1/synthesis-4.101.1.tar.gz"
-  sha256 "f5a26816afa836547191ae57a827df7dbe70ab361f9d1c51deabc653f238606d"
+  url "https://github.com/synthesisengineering/synthesis-skills/releases/download/v4.102.1/synthesis-4.102.1.tar.gz"
+  sha256 "b6a0a9107999c2315feb0a30e5104d03af8bb483e9cb32f2dcf5861bc7176285"
   license "Apache-2.0"
   depends_on "git"
   depends_on "python@3.12"
@@ -12,7 +12,7 @@ class Synthesis < Formula
       SYNTHESIS_BOOTSTRAP_PYTHON: Formula["python@3.12"].opt_bin/"python3.12"
   end
   test do
-    assert_match "4.101.1", shell_output("#{bin}/synthesis --version")
+    assert_match "4.102.1", shell_output("#{bin}/synthesis --version")
     assert_match "--profile", shell_output("#{bin}/synthesis --help")
   end
 end
