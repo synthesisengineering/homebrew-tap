0b3b0306b16917c21e61e4aeca882f9b418f32cd
Rajiv Pant <rajiv@rajiv.com>
2026-09-25 15:22:36 -0400

    Update package versions

--- Formula/synthesis-console.rb
+++ Formula/synthesis-console.rb
@@ -1,8 +1,8 @@
 class SynthesisConsole < Formula
   desc "Local project console for the Synthesis ecosystem"
   homepage "https://synthesiswork.org/download/"
-  url "https://github.com/synthesisengineering/synthesis-console/releases/download/v1.4.0/synthesis-console-1.4.0.tar.gz"
-  sha256 "9cea61507df525cdbf26c1f6935060b5c6e42661187f457288eb30ee486ed1fc"
+  url "https://github.com/synthesisengineering/synthesis-console/releases/download/v1.5.0/synthesis-console-1.5.0.tar.gz"
+  sha256 "01e843fbf0d52fa2d716f6262865e8b9730b914747c09b1af0dc00b59ef8f699"
   license "Apache-2.0"
   depends_on "oven-sh/bun/bun"
   depends_on "git"
@@ -14,7 +14,7 @@ class SynthesisConsole < Formula
       SYNTHESIS_BOOTSTRAP_PYTHON: Formula["python@3.12"].opt_bin/"python3.12"
   end
   test do
-    assert_match "1.4.0", shell_output("#{bin}/synthesis-console --version")
+    assert_match "1.5.0", shell_output("#{bin}/synthesis-console --version")
     assert_match "autostart", shell_output("#{bin}/synthesis-console --help")
   end
 end
--- Formula/synthesis.rb
+++ Formula/synthesis.rb
@@ -1,8 +1,8 @@
 class Synthesis < Formula
   desc "Verified installation and lifecycle for the Synthesis ecosystem"
   homepage "https://synthesiswork.org/download/"
-  url "https://github.com/synthesisengineering/synthesis-skills/releases/download/v4.102.1/synthesis-4.102.1.tar.gz"
-  sha256 "b6a0a9107999c2315feb0a30e5104d03af8bb483e9cb32f2dcf5861bc7176285"
+  url "https://github.com/synthesisengineering/synthesis-skills/releases/download/v4.149.1/synthesis-4.149.1.tar.gz"
+  sha256 "2950c3b68bf78d8723323787069e0f3e9171cf7ec5005ff57bb34e0cd97ff199"
   license "Apache-2.0"
   depends_on "git"
   depends_on "python@3.12"
@@ -12,7 +12,7 @@ class Synthesis < Formula
       SYNTHESIS_BOOTSTRAP_PYTHON: Formula["python@3.12"].opt_bin/"python3.12"
   end
   test do
-    assert_match "4.102.1", shell_output("#{bin}/synthesis --version")
+    assert_match "4.149.1", shell_output("#{bin}/synthesis --version")
     assert_match "--profile", shell_output("#{bin}/synthesis --help")
   end
 end
