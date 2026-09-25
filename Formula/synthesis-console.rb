d80b765d0b55b6a4c07708081b03367ccdf77f93
Rajiv Pant <rajiv@rajiv.com>
2026-09-25 18:10:35 -0400

    Update verified package versions

--- Formula/synthesis-console.rb
+++ Formula/synthesis-console.rb
@@ -1,8 +1,8 @@
 class SynthesisConsole < Formula
   desc "Local project console for the Synthesis ecosystem"
   homepage "https://synthesiswork.org/download/"
-  url "https://github.com/synthesisengineering/synthesis-console/releases/download/v1.5.0/synthesis-console-1.5.0.tar.gz"
-  sha256 "01e843fbf0d52fa2d716f6262865e8b9730b914747c09b1af0dc00b59ef8f699"
+  url "https://github.com/synthesisengineering/synthesis-console/releases/download/v1.5.1/synthesis-console-1.5.1.tar.gz"
+  sha256 "62e721b15f8697db95abfcf02985633ecb8236338938604981289eedd1572231"
   license "Apache-2.0"
   depends_on "oven-sh/bun/bun"
   depends_on "git"
@@ -14,7 +14,7 @@ class SynthesisConsole < Formula
       SYNTHESIS_BOOTSTRAP_PYTHON: Formula["python@3.12"].opt_bin/"python3.12"
   end
   test do
-    assert_match "1.5.0", shell_output("#{bin}/synthesis-console --version")
+    assert_match "1.5.1", shell_output("#{bin}/synthesis-console --version")
     assert_match "autostart", shell_output("#{bin}/synthesis-console --help")
   end
 end
